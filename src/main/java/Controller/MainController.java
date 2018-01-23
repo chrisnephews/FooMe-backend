0a42b53436fa408cc67d17597872f356989832ba
Chris Neven <c.k.neven@live.nl>
2018-01-23 13:54:49 +0100

    Added javadoc

--- src/main/java/Controller/MainController.java
+++ src/main/java/Controller/MainController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * Created by cknev on 10-1-2018.
+ * @author chrisneven
+ * The MainController class is responsible for handeling requests by using the DAO's for saving and the model for defining objects that are received.
+ * from the frontend. The Controller is the bridge between mobile application and the DAO -> database.
  */
 @Controller
 @RequestMapping(path="/")
